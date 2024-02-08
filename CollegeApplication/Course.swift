fa60e7eb22f844f1c83be8e15d13b1cbad6080dd
Jeremy <renhotsai@gmail.com>
2024-02-08 15:38:25 -0500

    feat: add func

--- CollegeApplication/Course.swift
+++ CollegeApplication/Course.swift
@@ -7,19 +7,37 @@
 
 import Foundation
 
-class Course : CustomStringConvertible{
+class Course : CustomStringConvertible, Identifiable{
     var id : UUID = UUID()
     var courseName : String = ""
     var courseDesc : String = ""
-    var instructor : String = ""
+    var instructorList : [String] = []
+    var studentList: [String] = []
+    
     
     var description: String{
-        return "id: \(id), Course Name: \(courseName), Course Desc: \(courseDesc), Instructor: \(instructor)"
+        return "id: \(id), Course Name: \(courseName), Course Desc: \(courseDesc), Instructors: \(instructorList)"
     }
-    
-    init(courseName: String, courseDesc: String, instructor: String) {
+    init(courseName: String, courseDesc: String){
         self.courseName = courseName
         self.courseDesc = courseDesc
-        self.instructor = instructor
+    }
+    
+    init(courseName: String, courseDesc: String, instructorList: [String]) {
+        self.courseName = courseName
+        self.courseDesc = courseDesc
+        self.instructorList = instructorList
+    }
+    
+    func addInstructor(instructor: String){
+        if !self.instructorList.contains(where:{$0 == instructor }){
+            self.instructorList.append(instructor)
+        }
+    }
+    
+    func addStudent(student:String){
+        if !self.studentList.contains(where:{$0 == student}){
+            self.studentList.append(student)
+        }
     }
 }

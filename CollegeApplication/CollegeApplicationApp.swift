163a8030fa16769ecc1b2e0b205b4747da589a05
Jeremy <renhotsai@gmail.com>
2024-02-08 11:32:30 -0500

    Initial Commit

---
+++ CollegeApplication/CollegeApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CollegeApplicationApp.swift
+//  CollegeApplication
+//
+//  Created by RENHO TSAI on 2024/2/8.
+//
+
+import SwiftUI
+
+@main
+struct CollegeApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CollegeApplication/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CollegeApplication
+//
+//  Created by RENHO TSAI on 2024/2/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

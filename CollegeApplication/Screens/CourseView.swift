ad9e4a3a7f8b6b6c87b396cec92152542dd0669d
HyunSeongLee <devhyun05@gmail.com>
2024-02-09 22:08:40 -0500

    login with error message, redirection to course displaying page

---
+++ CollegeApplication/Screens/CourseView.swift
@@ -0,0 +1,18 @@
+//
+//  CourseView.swift
+//  CollegeApplication
+//
+//  Created by 이현성 on 2/9/24.
+//
+
+import SwiftUI
+
+struct CourseView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CourseView()
+}

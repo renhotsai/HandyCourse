9bb59c5cbaa467d1c7795bf30db08be0449cb011
HyunSeongLee <devhyun05@gmail.com>
2024-02-11 23:45:13 -0500

    For ErrorCode, and MainView prototype navbar

---
+++ CollegeApplication/Screens/GradeView.swift
@@ -0,0 +1,18 @@
+//
+//  GradeView.swift
+//  CollegeApplication
+//
+//  Created by 이현성 on 2/11/24.
+//
+
+import SwiftUI
+
+struct GradeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GradeView()
+}

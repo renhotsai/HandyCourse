a12fcfcc113dc9c09e7c9af7cbb1a9639a03a421
Jeremy <renhotsai@gmail.com>
2024-02-14 17:02:06 -0500

    feat: start with WelcomeScreenView

--- CollegeApplication/CollegeApplicationApp.swift
+++ CollegeApplication/CollegeApplicationApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct CollegeApplicationApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            WelcomeScreenView()
         }
     }
 }

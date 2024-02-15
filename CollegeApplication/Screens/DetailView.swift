485b3e3766efe0a8b46b3200a89ec65e659b9023
HyunSeongLee <devhyun05@gmail.com>
2024-02-15 00:02:08 -0500

    For user interface updates

--- CollegeApplication/Screens/DetailView.swift
+++ CollegeApplication/Screens/DetailView.swift
@@ -9,53 +9,72 @@ import SwiftUI
 
 struct DetailView: View {
     
-    @EnvironmentObject var user : User
-    var course : Course
-    @State private var isActive :Bool = false
+    @EnvironmentObject var user: User
+    var course: Course
+    @State private var isActive: Bool = false
     @State private var alertMsg: Alert = Alert(title: Text(""))
     @State private var showAlert: Bool = false
     
     var body: some View {
         
-       VStack{
-            Text(course.courseDesc)
-            HStack{
-            Text("Instructor:")
-                ForEach(course.instructorList,id: \.self){ instructor in
-                    Text(instructor)
+        VStack {
+            if let imageName = course.courseImageName {
+                Image(imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(maxWidth: .infinity, maxHeight: 200)
+                    .clipped()
+            }
+            Text("Description: \(course.courseDesc)")
+                .font(.headline)
+            Text("Instructors:")
+                .font(.headline)
+            ForEach(course.instructorList, id: \.self) { instructor in
+                if let instructorObject = instructor as? Instructor {
+                    Text("- \(instructorObject.name)")
                 }
             }
+            Text("Start Date: \(formattedDate(course.startDate))")
+                .font(.headline)
+            Text("End Date: \(formattedDate(course.endDate))")
+                .font(.headline)
+       
+          
             Spacer()
             if user is Student {
                 let student = user as? Student
-                if student?.courseGrade[course.id.uuidString] == nil{
+                if student?.courseGrade[course.id.uuidString] == nil {
                     Button("Add Course", action: {
                         self.showAlert = true
-                        if (course.addStudent(student: student!)) {
+                        if course.addStudent(student: student!) {
                             student?.addCourse(courseId: course.id.uuidString)
                             self.isActive = true
-                            alertMsg = Alert(title: Text("Success"),message: Text("Successfully registered to the course"))
+                            alertMsg = Alert(title: Text("Success"), message: Text("Successfully registered to the course"))
                         } else {
-                            alertMsg = Alert(title: Text("Error"),message: Text("Error: Exceed max number of students"))
+                            alertMsg = Alert(title: Text("Error"), message: Text("Error: Exceed max number of students"))
                         }
                         
-                    }).navigationDestination(isPresented: $isActive){
-                        MainView().environmentObject(user)
-                    }
-                    .alert(isPresented: $showAlert, content: {
-                                        alertMsg
                     })
-                }else{
-                    Text("Grade: \(student?.courseGrade[course.id.uuidString] ?? 0 )")
+                    .padding()
+                    .alert(isPresented: $showAlert, content: {
+                        alertMsg
+                    })
+                } else {
+                    Text("This course added")
                 }
             }
         }
         .navigationTitle(course.courseName)
     }
+    func formattedDate(_ date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        return dateFormatter.string(from: date)
+    }
 }
 
-
-
-#Preview {
-    DetailView(course: Course(courseName: "C-Course", courseDesc: "Test C", studentLimit: 35, startDate: Date(), endDate: Date().addingTimeInterval(3600 * 24 * 12), instructorList: ["aaa", "bbb"])).environmentObject(User())
+struct DetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        DetailView(course: Course(courseName: "C-Course", courseDesc: "Test C", studentLimit: 35, startDate: Date(), endDate: Date().addingTimeInterval(3600 * 24 * 12), instructorList: ["aaa", "bbb"])).environmentObject(User())
+    }
 }
---
+++ CollegeApplication/Screens/StudentCourses/GradesView.swift
@@ -0,0 +1,18 @@
+//
+//  GradesView.swift
+//  CollegeApplication
+//
+//  Created by 이현성 on 2/14/24.
+//
+
+import SwiftUI
+
+struct GradesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GradesView()
+}

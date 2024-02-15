979b253de8975e62226da880bc21aa35a0ed3652
ichahal <aman.0002@icloud.com>
2024-02-15 03:48:57 -0500

    Made and integrateed about us and contact page.

---
+++ CollegeApplication/Screens/ContactView.swift
@@ -0,0 +1,18 @@
+//
+//  ContactView.swift
+//  CollegeApplication
+//
+//  Created by Aman  Chahal on 2/15/24.
+//
+
+import SwiftUI
+
+struct ContactView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContactView()
+}

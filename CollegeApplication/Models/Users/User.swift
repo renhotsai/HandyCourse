eede608a6dbde2ae079265f94456e7d451c8d4a2
Jeremy <renhotsai@gmail.com>
2024-02-26 22:53:49 -0500

    feat: update user remove username and password

--- CollegeApplication/Models/Users/User.swift
+++ CollegeApplication/Models/Users/User.swift
@@ -7,41 +7,26 @@
 
 import Foundation
 
-class User : Identifiable, ObservableObject{
+class User : Identifiable, ObservableObject, Codable{
     var id : UUID = UUID()
     var name: String
-    var username: String
-    var password: String
     var email:String = ""
     var address: String = ""
     var phoneNumber: String = ""
     var imageName: String?
     
-    init(name: String, username: String, password: String, email: String, address: String, phoneNumber: String, imageName: String) {
+    init(name: String, email: String, address: String, phoneNumber: String, imageName: String) {
         self.name = name
-        self.username = username
-        self.password = password
         self.email = email
         self.address = address
         self.phoneNumber = phoneNumber
         self.imageName = imageName
     }
     
-    init(name: String, username: String, password: String, email: String, address: String, phoneNumber: String) {
+    init(name: String, email: String, address: String, phoneNumber: String) {
         self.name = name
-        self.username = username
-        self.password = password
         self.email = email
         self.address = address
         self.phoneNumber = phoneNumber
-    }
-    
-    init(){
-        self.name = "NA"
-        self.username = "NA"
-        self.password = "NA"
-        self.email = "NA"
-        self.address = "NA"
-        self.phoneNumber = "NA"
     }
 }
